b0d86243d94145352bbaba9a14d33527ce356291
Mustapha Mohammed <mustaphamohammed0705@gmail.com>
2024-10-02 18:14:45 +0000

    updated

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,16 +1,15 @@
 export default function createIteratorObject(report) {
-    const allEmployees = report.allEmployees;
-  
-    function* employeeIterator() {
-      for (const department in allEmployees) {
-        if (allEmployees.hasOwnProperty(department)) { // Ensure it's the object's own property
-          for (const employee of allEmployees[department]) {
-            yield employee;
-          }
+  const { allEmployees } = report;
+
+  function* employeeIterator() {
+    for (const department in allEmployees) {
+      if (allEmployees.hasOwnProperty(department)) { // Ensure it's the object's own property
+        for (const employee of allEmployees[department]) {
+          yield employee;
         }
       }
     }
-  
-    return employeeIterator();
   }
-  
+
+  return employeeIterator();
+}
